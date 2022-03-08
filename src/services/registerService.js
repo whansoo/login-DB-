8607f01feb35e5c09a1228b40e7fa843077a194c
whansoo <ghkstn7523@naver.com>
2022-03-08 21:18:49 +0900

    회원가입 비밀번호 암호화

--- src/services/registerService.js
+++ src/services/registerService.js
@@ -1,8 +1,21 @@
 import connection from "../configs/connectDB"
+import bcrypt from "bcryptjs";
+
 let createNewUser = (user) => {
     return new Promise((resolve, reject) => {
         try {
-            connection.query( "INSERT INTO users set?", user, function(error, rows) {
+
+           //hash user's password
+           let salt =  bcrypt.genSaltSync(10);
+           let data = {
+               fullname: user.fullname,
+               email: user.email,
+               password: bcrypt.hashSync(user.password, salt)
+           };
+
+
+           //create a new user
+            connection.query( "INSERT INTO users set?", data, function(error, rows) {
                 if(error) reject(error);
                 resolve("create a new user successfully");
             })
